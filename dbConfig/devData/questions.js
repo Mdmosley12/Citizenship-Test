a96fe5ae44782c225582729a9c43d65a2d087796
Mdmosley12 <mdmosley12@gmail.com>
2023-04-06 16:42:09 +0100

    add devData

---
+++ dbConfig/devData/questions.js
@@ -0,0 +1,90 @@
+module.exports = [
+  {
+    question_text: "What is the name of the UK's flag?",
+  },
+  {
+    question_text: "What is the name of the national anthem of the UK?",
+  },
+  {
+    question_text: "Who is the head of state of the UK?",
+  },
+  {
+    question_text: "What is the name of the highest court in the UK?",
+  },
+  {
+    question_text: "When is St. George's Day celebrated in the UK?",
+  },
+  {
+    question_text: "What is the name of the highest mountain in the UK?",
+  },
+  {
+    question_text: "What is the capital city of Northern Ireland?",
+  },
+  {
+    question_text: "What is the name of the famous clock tower in London?",
+  },
+  {
+    question_text: "What is the name of the UK's currency?",
+  },
+  {
+    question_text:
+      "What is the name of the famous London park where the Queen's official residence is located?",
+  },
+  {
+    question_text:
+      "What is the name of the building that houses the UK parliament?",
+  },
+  {
+    question_text: "What is the name of the famous university in Cambridge?",
+  },
+  {
+    question_text: "What is the name of the cathedral in Canterbury?",
+  },
+  {
+    question_text:
+      "What is the name of the playwright who wrote 'Romeo and Juliet'?",
+  },
+  {
+    question_text:
+      "What is the name of the detective created by Sir Arthur Conan Doyle?",
+  },
+  {
+    question_text:
+      "What is the name of the scientist who discovered the laws of motion and gravity?",
+  },
+  {
+    question_text:
+      "What is the name of the war that was fought between 1939 and 1945?",
+  },
+  {
+    question_text:
+      "What was the name of the lead singer of The Beatles that was murdered in 1980?",
+  },
+  {
+    question_text:
+      "What is the name of the British author who wrote 'Harry Potter'?",
+  },
+  {
+    question_text:
+      "What is the name of the scientist who discovered penicillin?",
+  },
+  {
+    question_text:
+      "What is the name of the monarch who ruled for 63 years in the 19th century?",
+  },
+  {
+    question_text:
+      "What is the name of the British comedian who starred in 'Mr. Bean'?",
+  },
+  {
+    question_text:
+      "What is the name of the famous London street known for its luxury shopping?",
+  },
+  {
+    question_text: "What is the name of the river that flows through London?",
+  },
+  {
+    question_text:
+      "What is the name of the Prime Minister who led the UK during World War II?",
+  },
+];
